62c33ca9a8e04046c1956400a48f0fc30cc518d1
YuliiaShpytsia <yulia93shpiza@gmail.com>
2024-10-13 18:39:31 +0300

    intro commit

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  readonly name: string;
+  readonly age: number;
+  readonly email: string;
+  readonly password: string;
+  readonly role: string;
+}
